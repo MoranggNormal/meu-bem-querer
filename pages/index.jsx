dbad6d27d49c131639fed179b6d611b19ddb2592
Euller Peixoto <peixotoeuller500@gmail.com>
2021-12-20 02:50:55 -0300

    setup project

---
+++ pages/index.jsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Meu bem-querer | Página Inicial</title>
+        <meta name="description" content="Meu bem-querer" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+
+    </div>
+  )
+}
