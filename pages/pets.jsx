d4e47efbd5540dd46cc7590db281dbd3ce01bbab
Euller Peixoto <peixotoeuller500@gmail.com>
2022-01-24 23:25:07 -0200

    fix page crashing

--- pages/pets.jsx
+++ pages/pets.jsx
@@ -7,78 +7,78 @@ const db = firebase.firestore();
 const petRef = db.collection("pets");
 
 const Pets = ({ data }) => {
- const db = firebase.firestore();
- const petRef = db.collection("pets");
+  const db = firebase.firestore();
+  const petRef = db.collection("pets");
 
- const [petData, setPetData] = useState([]);
- const [loading, setLoading] = useState(true);
+  const [petData, setPetData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
- useEffect(() => {
-  petRef
-   .where("pending", "==", false)
-   .orderBy("upVote", "desc")
-   .onSnapshot((querySnapshot) => {
-    let list = [];
-    querySnapshot.forEach((doc) => {
-     list.push({ dbId: doc.id, data: doc.data() });
-    });
-    setPetData(list);
-    setLoading(false);
-   });
- }, [petRef]);
+  useEffect(() => {
+    petRef
+      .where("pending", "==", false)
+      .orderBy("upVote", "desc")
+      .onSnapshot((querySnapshot) => {
+        let list = [];
+        querySnapshot.forEach((doc) => {
+          list.push({ dbId: doc.id, data: doc.data() });
+        });
+        setPetData(list);
+        setLoading(false);
+      });
+  }, []);
 
- return (
-  <div>
-   <Grid container component="section">
-    {data &&
+  return (
+    <div>
+      <Grid container component="section">
+        {data &&
           petData.map(({ dbId, data }) => {
-           return (
-            <Grid item key={data.petUid} xs={12} sm={6} md={4} lg={3}>
-             <RecipeReviewCard
-              petName={data.petName}
-              added={data.added}
-              authorName={data.authorName}
-              authorPhoto={data.authorPhoto}
-              petDescription={data.petDescription}
-              petImg={data.petImg}
-              petSituation={data.petSituation}
-              petRace={data.petRace}
-              petCity={data.petCity}
-              petState={data.petState}
-              upVote={data.upVote}
-              upVotes={data.upVotes}
-              petUid={data.petUid}
-              dbId={dbId}
-              loading={loading}
-             />
-            </Grid>
-           );
+            return (
+              <Grid item key={data.petUid} xs={12} sm={6} md={4} lg={3}>
+                <RecipeReviewCard
+                  petName={data.petName}
+                  added={data.added}
+                  authorName={data.authorName}
+                  authorPhoto={data.authorPhoto}
+                  petDescription={data.petDescription}
+                  petImg={data.petImg}
+                  petSituation={data.petSituation}
+                  petRace={data.petRace}
+                  petCity={data.petCity}
+                  petState={data.petState}
+                  upVote={data.upVote}
+                  upVotes={data.upVotes}
+                  petUid={data.petUid}
+                  dbId={dbId}
+                  loading={loading}
+                />
+              </Grid>
+            );
           })}
-   </Grid>
-  </div>
- );
+      </Grid>
+    </div>
+  );
 };
 
 let data = [];
 
 export function getStaticProps() {
- try {
-  petRef
-   .where("pending", "==", false)
-   .orderBy("upVote", "desc")
-   .onSnapshot((querySnapshot) => {
-    data = [];
-    querySnapshot.forEach((doc) => {
-     data.push({ dbId: doc.id, data: doc.data() });
-    });
-   });
- } catch (error) {
-  console.log(error);
- } finally {
-  return {
-   props: { data },
-  };
- }
+  try {
+    petRef
+      .where("pending", "==", false)
+      .orderBy("upVote", "desc")
+      .onSnapshot((querySnapshot) => {
+        data = [];
+        querySnapshot.forEach((doc) => {
+          data.push({ dbId: doc.id, data: doc.data() });
+        });
+      });
+  } catch (error) {
+    console.log(error);
+  } finally {
+    return {
+      props: { data },
+    };
+  }
 }
 
 export default Pets;
