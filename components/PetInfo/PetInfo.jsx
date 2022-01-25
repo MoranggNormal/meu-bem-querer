146d406ce5ee3c00ee08b43aa3ffbdb04b5f4a57
Euller Peixoto <peixotoeuller500@gmail.com>
2022-01-25 00:03:05 -0200

    update pre-commit script

--- components/PetInfo/PetInfo.jsx
+++ components/PetInfo/PetInfo.jsx
@@ -20,141 +20,141 @@ import Link from "next/link";
 - Styles
 */
 const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  bgcolor: "background.paper",
-  boxShadow: 24,
-  display: "flex",
-  width: "60em",
-  height: "25em",
-  overflow: "hidden",
+ position: "absolute",
+ top: "50%",
+ left: "50%",
+ transform: "translate(-50%, -50%)",
+ bgcolor: "background.paper",
+ boxShadow: 24,
+ display: "flex",
+ width: "60em",
+ height: "25em",
+ overflow: "hidden",
 };
 
 /*
 - Typograhpy Style
 */
 const typography = {
-  display: "flex",
-  alignItems: "center",
-  gap: "0.3em",
-  color: "#616161",
-  wordBreak: "break-word",
+ display: "flex",
+ alignItems: "center",
+ gap: "0.3em",
+ color: "#616161",
+ wordBreak: "break-word",
 };
 
 export default function BasicModal({
-  petName,
-  petImg,
-  added,
-  petRace,
-  petSituation,
-  petDescription,
-  petUid,
+ petName,
+ petImg,
+ added,
+ petRace,
+ petSituation,
+ petDescription,
+ petUid,
 }) {
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+ const [open, setOpen] = useState(false);
+ const handleOpen = () => setOpen(true);
+ const handleClose = () => setOpen(false);
 
-  return (
-    <div>
-      <Button sx={{ gap: "0.4em" }} onClick={handleOpen}>
-        <InfoIcon /> Detalhes
-      </Button>
-      <Link
-        href={{
-          pathname: "pets/[pet]",
-          query: {
-            pet: petUid,
-            petName,
-          },
-        }}
-      >
+ return (
+  <div>
+   <Button sx={{ gap: "0.4em" }} onClick={handleOpen}>
+    <InfoIcon /> Detalhes
+   </Button>
+   <Link
+    href={{
+     pathname: "pets/[pet]",
+     query: {
+      pet: petUid,
+      petName,
+     },
+    }}
+   >
         Gogo
-      </Link>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
+   </Link>
+   <Modal
+    open={open}
+    onClose={handleClose}
+    aria-labelledby="modal-modal-title"
+    aria-describedby="modal-modal-description"
+   >
+    <Grid container component={Paper} sx={style}>
+     <Grid item xs={4} component="article">
+      <Box
+       component="img"
+       sx={{
+        height: "100%",
+        width: "100%",
+        objectFit: "cover",
+       }}
+       src={petImg}
+       alt={petName}
+      />
+     </Grid>
+
+     <Grid item xs={8} component="article" sx={{ p: 2 }}>
+      <div
+       style={{
+        display: "flex",
+        justifyContent: "space-between",
+        marginBottom: "2em",
+       }}
       >
-        <Grid container component={Paper} sx={style}>
-          <Grid item xs={4} component="article">
-            <Box
-              component="img"
-              sx={{
-                height: "100%",
-                width: "100%",
-                objectFit: "cover",
-              }}
-              src={petImg}
-              alt={petName}
-            />
-          </Grid>
-
-          <Grid item xs={8} component="article" sx={{ p: 2 }}>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "space-between",
-                marginBottom: "2em",
-              }}
-            >
-              <div style={{ display: "flex", gap: "0.4em" }}>
-                <Typography variant="h6" component="h2" sx={typography}>
-                  <InfoIcon />
+       <div style={{ display: "flex", gap: "0.4em" }}>
+        <Typography variant="h6" component="h2" sx={typography}>
+         <InfoIcon />
                   Informações sobre
-                </Typography>
+        </Typography>
 
-                <Typography variant="h5" component="h3" sx={typography}>
-                  <u>{petName}</u>
-                </Typography>
-              </div>
-              <Typography component="small" sx={typography}>
-                <CalendarTodayIcon /> {added}
-              </Typography>
-            </div>
+        <Typography variant="h5" component="h3" sx={typography}>
+         <u>{petName}</u>
+        </Typography>
+       </div>
+       <Typography component="small" sx={typography}>
+        <CalendarTodayIcon /> {added}
+       </Typography>
+      </div>
 
-            <div style={{ display: "flex", gap: "3.5em", marginBottom: "5em" }}>
-              <Typography id="modal-modal-title" component="p" sx={typography}>
+      <div style={{ display: "flex", gap: "3.5em", marginBottom: "5em" }}>
+       <Typography id="modal-modal-title" component="p" sx={typography}>
                 Raça: {petRace}
-              </Typography>
+       </Typography>
 
-              <Typography
-                id="modal-modal-title"
-                component="p"
-                noWrap={true}
-                sx={typography}
-              >
+       <Typography
+        id="modal-modal-title"
+        component="p"
+        noWrap={true}
+        sx={typography}
+       >
                 Situação: {petSituation}
-              </Typography>
-            </div>
+       </Typography>
+      </div>
 
-            <Typography
-              id="modal-modal-title"
-              variant="h6"
-              component="h4"
-              sx={typography}
-            >
+      <Typography
+       id="modal-modal-title"
+       variant="h6"
+       component="h4"
+       sx={typography}
+      >
               Um pouco mais sobre {petName}:
-              <br />
-            </Typography>
-            <Typography id="modal-modal-title" component="p" sx={typography}>
-              {petDescription}
-            </Typography>
+       <br />
+      </Typography>
+      <Typography id="modal-modal-title" component="p" sx={typography}>
+       {petDescription}
+      </Typography>
 
-            <div
-              style={{
-                position: "absolute",
-                bottom: "1.5em",
-                right: "1.5em",
-              }}
-            >
-              <Button variant="outlined">Eu quero adotar!</Button>
-            </div>
-          </Grid>
-        </Grid>
-      </Modal>
-    </div>
-  );
+      <div
+       style={{
+        position: "absolute",
+        bottom: "1.5em",
+        right: "1.5em",
+       }}
+      >
+       <Button variant="outlined">Eu quero adotar!</Button>
+      </div>
+     </Grid>
+    </Grid>
+   </Modal>
+  </div>
+ );
 }
